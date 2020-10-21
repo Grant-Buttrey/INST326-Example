945fd41a794d88b68441fa42bc1c821d4cd45b89
Grant-Buttrey <generalbuttrey@gmail.com>
2020-10-21 11:24:10 -0400

    Created changes to packages and moduels
    Grant created a packages with subpackages and an example script.

---
+++ my_project/people.py
@@ -0,0 +1,6 @@
+def student_information(name, age = 20):
+    print(f"Hi my name is {name}, and I am {age} years old. ")
+    
+    
+if __name__ == "__main__":
+    student_information("Gabriel", age = 40)
---
+++ my_project/presentation.py
@@ -0,0 +1,12 @@
+# import information.people.students as module2
+# from information.people.students import student_information
+from information.people.students import *
+# import people
+from people import *
+
+
+
+if __name__ == "__main__":
+    print("Hi i am currently in the presentation script")
+    student_information("Tim", age = 30)
+    people.student_information("Joe", age = 25)
